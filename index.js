585468b8d215d5236e323a9e55f7029bea2662ea
Ross <rossudenko@gmail.com>
2025-11-30 07:09:11 +0000

    Update

---
+++ index.js
@@ -0,0 +1,24 @@
+document.getElementById("firstwordinthejavascriptsection").textContent = "I change this word into Goodbye";
+document.getElementById("firstletter").textContent = "a b c d e f g h i j k l m n o p q r s t u v w y z";
+
+// ----------------------------------------------------
+
+console.log("I like Pizza");
+console.log("I like play football");
+
+// ----------------------------------------------------
+// VARIABLES
+// These are all numbers types
+let x = 100 
+let y = 5.99
+let z = 60 / 89
+
+console.log(x, y, z);
+console.log(typeof x, typeof y, typeof z);
+
+// String variables can be numbers but javascript cant do math with them.
+let firstName = "ross!"
+let email = "rostyslavsudenko123@gmail.com"
+let age = "25"
+
+console.log(firstName, email, age);
